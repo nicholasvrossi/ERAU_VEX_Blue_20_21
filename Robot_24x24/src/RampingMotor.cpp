40ad374c241f5e9b1b646e58bb98c6ff87ad825f
danielstruck <struckd@my.erau.edu>
2020-09-30 17:30:50 -0700

    Fixed linking errors for Robot_24x24

---
+++ Robot_24x24/src/RampingMotor.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include "main.h"
+#include "RampingMotor.hpp"
+#include "Tools.hpp"
+
+
+void pros::RampingMotor::setup(float rampStep) {
+    this->rampStep = rampStep;
+    this->currentSpeed = 0;
+}
+
+
+pros::RampingMotor::RampingMotor(const std::uint8_t port,
+                                 const pros::motor_gearset_e_t gearset,
+                                 bool reverse,
+                                 pros::motor_encoder_units_e_t encoder_units,
+                                 float rampStep)
+                                 : pros::Motor(port, gearset, reverse, encoder_units) {
+    setup(rampStep);
+ }
+pros::RampingMotor::RampingMotor(const std::uint8_t port,
+                                 const pros::motor_gearset_e_t gearset,
+                                 const bool reverse,
+                                 float rampStep)
+                                 : pros::Motor(port, gearset, reverse) {
+    setup(rampStep);
+}
+pros::RampingMotor::RampingMotor(const std::uint8_t port,
+                                 const pros::motor_gearset_e_t gearset,
+                                 float rampStep)
+                                 : pros::Motor(port, gearset) {
+    setup(rampStep);
+}
+pros::RampingMotor::RampingMotor(const std::uint8_t port,
+                                 const bool reverse,
+                                 float rampStep)
+                                 : pros::Motor(port, reverse) {
+    setup(rampStep);
+}
+pros::RampingMotor::RampingMotor(const std::uint8_t port,
+                                 float rampStep)
+                                 : pros::Motor(port) {
+    setup(rampStep);
+}
+
+
+std::int32_t pros::RampingMotor::move(const std::int32_t voltage)  {
+    std::int32_t targetSpeed = std::clamp((int) voltage, -127, 127);
+
+    int targetDisplacement = targetSpeed - currentSpeed;
+    int targetDirection = 0;
+
+    if (targetDisplacement != 0) {
+        int targetDistance = std::abs(targetDisplacement);
+        if (targetDistance < rampStep) {
+            this->currentSpeed = targetSpeed;
+        }
+        else {
+            targetDirection = sign(targetDisplacement);
+            this->currentSpeed += targetDirection * rampStep;
+        }
+        move(currentSpeed);
+    }
+
+    return 1;
+}
--- Robot_24x24/src/motor_def.cpp
+++ Robot_24x24/src/motor_def.cpp
@@ -2,13 +2,33 @@
 #include "main.h"
 #include "../../G_inc/headers/InvKinematics.hpp"
 
-//pros::Controller master = pros:Controller(pros::E_CONTROLLER_MASTER);
+pros::Controller master = pros::Controller(pros::E_CONTROLLER_MASTER);
 
 
+pros::Motor frontLeft = pros::Motor(MOTOR_FRONT_LEFT_PORT,
+                      pros::E_MOTOR_GEARSET_18,
+                      false,
+                      pros::E_MOTOR_ENCODER_COUNTS);
 
-/*pros::MotorSet driveMotors{
+pros::Motor frontRight = pros::Motor(MOTOR_FRONT_RIGHT_PORT,
+                      pros::E_MOTOR_GEARSET_18,
+                      true,
+                      pros::E_MOTOR_ENCODER_COUNTS);
+
+pros::Motor backLeft = pros::Motor(MOTOR_BACK_LEFT_PORT,
+                      pros::E_MOTOR_GEARSET_18,
+                      false,
+                      pros::E_MOTOR_ENCODER_COUNTS);
+
+pros::Motor backRight = pros::Motor(MOTOR_BACK_RIGHT_PORT,
+                      pros::E_MOTOR_GEARSET_18,
+                      true,
+                      pros::E_MOTOR_ENCODER_COUNTS);
+
+
+pros::MotorSet driveMotors{
   frontLeft,
   frontRight,
   backLeft,
   backRight
-};*/
+};
